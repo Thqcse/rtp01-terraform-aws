34a9bae5b12eae738638150ad0d1f36fb639bd70
Mohamed Thoufeek <karadipayyan@gmail.com>
2022-07-23 15:35:06 +0530

    Update ec2.tf

--- modules/ec2/ec2.tf
+++ modules/ec2/ec2.tf
@@ -1,10 +1,10 @@
 resource "aws_instance" "myec2" {
-  ami = "ami-0f9fc25dd2506cf6d"
+  ami = "ami-0cff7528ff583bf9a"
   instance_type = var.instance_type
   associate_public_ip_address = var.associate_ip
   key_name = var.key_name
   vpc_security_group_ids = var.vpc_security_group_ids
-  //security_groups = var.security_groups
+  security_groups = var.security_groups
   for_each = toset(["master", "slave"])
   tags = {
     Name = "${each.key}"
